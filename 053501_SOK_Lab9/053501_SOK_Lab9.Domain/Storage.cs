e070b29c8a5189a581bca447861f2fc513b2d858
LEINADALIEN <79170882+leinadalien@users.noreply.github.com>
2021-12-10 12:05:10 +0300

    Add files via upload

---
+++ 053501_SOK_Lab9/053501_SOK_Lab9.Domain/Storage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _053501_SOK_Lab9.Domain
+{
+    public class Storage
+    {
+        
+        public List<Detail> Details { get; set; }
+        public string Address { get; set; }
+        public int Capasity { get; set; }
+        public Storage()
+        {
+            Details = new();
+            Capasity = 10;
+            Address = null;
+        }
+        public Storage(int capasity, string address)
+        {
+            Details = new();
+            this.Capasity = capasity;
+            Address = address;
+        }
+        public void AddDetail(Detail detail)
+        {
+            if(Capasity > Details.Count)
+            {
+                Details.Add(detail);
+            } else
+            {
+                throw new OverflowException("The storage is full");
+            }
+        }
+        public void RemoveDetail(Detail detail)
+        {
+            if (Details.Count > 0)
+            {
+                if(Details.Contains(detail))
+                {
+                    Details.Remove(detail);
+                } else
+                {
+                    throw new ArgumentException("The storage hasn't such element");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("The storage is empty");
+            }
+        }
+    }
+}
