fe724d93d6d3a583bdfc4531cc44581d1070447c
LEINADALIEN <79170882+leinadalien@users.noreply.github.com>
2021-12-10 11:59:52 +0300

    Add files via upload

---
+++ 053501_SOK_Lab9/053501_SOK_Lab9.Domain/Factory.cs
@@ -0,0 +1,24 @@
+﻿namespace _053501_SOK_Lab9.Domain
+{
+    public class Factory
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Storage Storage { get; set; }
+        public Factory()
+        {
+            Name = null;
+            Description = null;
+            Address = null;
+            Storage = null;
+        }
+        public Factory(string name, string description, string address, Storage storage)
+        {
+            Name = name;
+            Description = description;
+            Address = address;
+            Storage = storage;
+        }
+    }
+}
